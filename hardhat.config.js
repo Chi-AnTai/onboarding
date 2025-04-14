bf37a3d2b01c7302fe1851ae876cc32d87a76bec
Chi-AnTai <andy.chian.tai@gmail.com>
2025-04-14 13:38:49 +0800

    feat: update config and remove unused template

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,17 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    compilers: [
+        {
+            version: "0.7.6",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 1,
+                },
+            },
+        },
+    ],
+},
 };
